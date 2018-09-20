3c1c56c198014a2acbd77c0298032d3a833acc1b
Carles Andres <carlos.andres.c@gmail.com>
2018-09-20 13:32:53 +0100

    Removed linting errors from nightwatch files

--- test/browser/config/nightwatch-local.js
+++ test/browser/config/nightwatch-local.js
@@ -18,7 +18,6 @@ if(process.env.CI) {
 			console.log("Finished running single nightwatch test.");
 		});
 	});
-	return;
 }
 
 require('dotenv').load();
@@ -38,7 +37,7 @@ try {
 	})
 
 	process.mainModule.filename = "./node_modules/.bin/nightwatch"
-	
+
 	// Code to start browserstack local before start of test
 	console.log("Connecting BrowserStackLocal tunnel...");
 	Nightwatch.bs_local = bs_local = new browserstack.Local();
